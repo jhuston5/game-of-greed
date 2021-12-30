93770ad5aa3acf5deb9e642b93934c47cfe2acec
Joshua Huston <joshua.david.huston@gmail.com>
2021-12-30 12:44:42 -0500

    All tests passing, refactoring needed

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -10,7 +10,8 @@ class Game:
   current_round = 0
   def __init__(self, current_dice_count=6):
     self.current_dice_count = current_dice_count
-
+    # Bring in an instance of the Banker Class to store scores
+    self.banker = Banker()
 
  # roller_ex = (4,4,5,2,3,1)
   roller_str = ''
@@ -31,29 +32,30 @@ class Game:
       print(f"Starting round {current_round}")
       self.new_roll(current_dice_count)
 
+      # While true here?
       print("Enter dice to keep, or (q)uit:") 
-      round_response = input("> ")
-      if round_response == "q": 
+      dice_response = input("> ")
+      if dice_response == "q": 
         self.end_game()
 
-      print(f'Round Response: {round_response}')
-      unbanked = GameLogic.calculate_score(round_response)
-      print(f'This is unbanked {unbanked}')
-      """dice_remaining = self.current_dice_count - len(round_response)
-      print(f"You have {unbanked} unbanked points and {dice_remaining} remaining")"""
-    # print("(r)oll again, (b)ank your points or (q)uit:")
-    
-    # round_response = input("> ")
-    # if round_response = "r":
-    #   start_round(current_dice_count)
-    # if round_response = "b":
-    #   Banker.bank.balance += unbanked
-    #   print(f"You banked {unbanked} points in round {current_round}")
-    #   current_round += 1
-    #   print(f"Total score is {Banker.bank.balance} points")
+      # print(f'Round Response: {type(dice_response)}')
+      unbanked = GameLogic.calculate_score(dice_response)
+      # print(f'This is unbanked {unbanked}')
+      dice_remaining = self.current_dice_count - len(dice_response)
+      print(f"You have {unbanked} unbanked points and {dice_remaining} dice remaining")
+      print("(r)oll again, (b)ank your points or (q)uit:")
+      
+      round_response = input("> ")
+      if round_response == "r":
+        self.start_round(current_dice_count)
+      if round_response == "b":
+        self.banker.balance += unbanked
+        print(f"You banked {unbanked} points in round {current_round}")
+        current_round += 1
+        print(f"Total score is {self.banker.balance} points")
     #   start_round()
-    # if round_response = "q":
-    #   end_game()
+        if round_response == "q":
+          self.end_game()
 
 
    
@@ -62,6 +64,7 @@ class Game:
     while True:
       self.start_round(current_round,self.current_dice_count)
       current_round += 1
+      
     # roll = GameLogic.roll_dice(current_dice_count)
 
     print("Enter dice to keep, or (q)uit:") 
@@ -87,7 +90,7 @@ class Game:
     print("OK. Maybe another time")
 
   def end_game(self): 
-    print(f'Thanks for playing. You earned 0 points')
+    print(f'Thanks for playing. You earned {self.banker.balance} points')
     sys.exit()
   
   
@@ -111,9 +114,11 @@ class GameLogic:
     def calculate_score(dice_list):
       # Set semi-global current_roll_score storage variable
       current_roll_score = 0
-      print((dice_list))
+      # print((dice_list))
+      roll_resp = str(dice_list)
+      # print(roll_resp)
       count_roll = Counter(dice_list)
-      print(f'this is count roll: {count_roll}')
+      # print(f'this is count roll: {count_roll}')
       
 
     # Check if there is a straight roll
@@ -125,34 +130,38 @@ class GameLogic:
     # Iterate through the roll to determine its score
       pairs = 0
       for i in count_roll:
-          count_roll_type = count_roll.keys()
-          print(f'Count Roll Type: {count_roll_type}')
-          print(f'This is i of count roll: {i}')
-          print(f'This is count_roll[i]: {count_roll[i]}')
-        # Check for pairs of numbers
-          if count_roll[i] == 2:
-            pairs += count_roll[i]
-            if pairs == 6:
-              current_roll_score += 1500
-        
-        # Check for rolls of 5 less than 3 of a kind
-          if i == 5 and count_roll[i] < 3:
-            score = count_roll[i] * 50
-            print(f'Score: {score}')
-            current_roll_score += score
+        # Set the counter dictionary key as an integer for comparison eg {'5': 1} i = int('5')
+        set_i_int = int(i)
+        # Testing to try and see the object issue
+        """count_roll_type = count_roll.keys()
+        print(f'Count Roll Type: {count_roll_type}')
+        print(f'This is i of count roll: {type(i)}')
+        print(f'This is count_roll[i]: {count_roll[i]}')"""
+      # Check for pairs of numbers
+        if count_roll[i] == 2:
+          pairs += count_roll[i]
+          if pairs == 6:
+            current_roll_score += 1500
+      
+      # Check for rolls of 5 less than 3 of a kind
+      # Threw in an extra condition to handle if the dict obj is a str (or i == '5)
+        if set_i_int == 5 and count_roll[i] < 3:
+          score = count_roll[i] * 50
+          # print(f'Score: {score}')
+          current_roll_score += score
 
-        # Check for rolls of 1 less than 3 of a kind
-          elif i == 1 and count_roll[i] < 3:
-            current_roll_score += count_roll[i] * 100
-        
-        # Write a function that iterates through here and doubles it
-        # Deal with rolls of 1 more than 3 of a kind
-          elif i == 1 and count_roll[i] >= 3:
-            current_roll_score += ((count_roll[i] - 2) * 1000)
-          elif count_roll[i] >= 3:
-            current_roll_score += ((i * 100) * (count_roll[i] - 2))        
+      # Check for rolls of 1 less than 3 of a kind
+        elif set_i_int == 1 and count_roll[i] < 3:
+          current_roll_score += count_roll[i] * 100
+      
+      # Write a function that iterates through here and doubles it
+      # Deal with rolls of 1 more than 3 of a kind
+        elif set_i_int == 1 and count_roll[i] >= 3:
+          current_roll_score += ((count_roll[i] - 2) * 1000)
+        elif count_roll[i] >= 3:
+          current_roll_score += ((set_i_int * 100) * (count_roll[i] - 2))        
 
-      print(f'this is current roll score: {current_roll_score}')
+      # print(f'this is current roll score: {current_roll_score}')
       return current_roll_score      
  
 class Banker:
